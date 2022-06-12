99b2b7b91e54f1510639734c1f3df71fa7f399e8
Carl Combrinck <carljcombrinck@gmail.com>
2022-06-12 22:05:22 +0200

    Add class to represent knowledge

---
+++ src/main/java/com/mbdr/utils/parsing/KnowledgeBase.java
@@ -0,0 +1,48 @@
+package com.mbdr.utils.parsing;
+
+import org.tweetyproject.logics.pl.syntax.PlBeliefSet;
+import org.tweetyproject.logics.pl.syntax.PlFormula;
+
+public class KnowledgeBase {
+    
+    private PlBeliefSet defeasibleKnowledge;
+    private PlBeliefSet propositionalKnowledge;
+
+    public KnowledgeBase(){
+        this.defeasibleKnowledge = new PlBeliefSet();
+        this.propositionalKnowledge = new PlBeliefSet();
+    }
+
+    public KnowledgeBase(PlBeliefSet defeasibleKnowledge, PlBeliefSet propositionalKnowledge){
+        this.defeasibleKnowledge = new PlBeliefSet(defeasibleKnowledge);
+        this.propositionalKnowledge = new PlBeliefSet(propositionalKnowledge);
+    }
+
+    public void addDefeasibleFormula(PlFormula formula){
+        this.defeasibleKnowledge.add(formula);
+    }
+
+    public void addPropositionalFormula(PlFormula formula){
+        this.propositionalKnowledge.add(formula);
+    }
+
+    public PlBeliefSet getDefeasibleKnowledge(){
+        return new PlBeliefSet(this.defeasibleKnowledge);
+    }
+
+    public PlBeliefSet getPropositionalKnowledge(){
+        return new PlBeliefSet(this.propositionalKnowledge);
+    }
+
+    public PlBeliefSet union(){
+        return union(this.propositionalKnowledge, this.defeasibleKnowledge);
+    }
+
+    public static PlBeliefSet union(PlBeliefSet a, PlBeliefSet b){
+        PlBeliefSet temp = new PlBeliefSet();
+        temp.addAll(a);
+        temp.addAll(b);
+        return temp;
+    }
+
+}
