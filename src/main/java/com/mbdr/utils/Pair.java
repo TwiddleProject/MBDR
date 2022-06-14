c2f34e1e5e8b3aede66200bdb769d62d19948cd1
Carl Combrinck <carljcombrinck@gmail.com>
2022-06-14 21:17:03 +0200

    Create generic Pair class

---
+++ src/main/java/com/mbdr/utils/Pair.java
@@ -0,0 +1,30 @@
+package com.mbdr.utils;
+
+/**
+* https://stackoverflow.com/questions/6044923/generic-pair-class
+*/
+public class Pair<F, S> {
+    private F first;
+    private S second;
+
+    public Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public void setFirst(F first) {
+        this.first = first;
+    }
+
+    public void setSecond(S second) {
+        this.second = second;
+    }
+
+    public F getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+}
