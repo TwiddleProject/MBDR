6ad761e9de8f84c711174806c8175b794d2b7fa1
Jaron Cohen <jdicohen@gmail.com>
2022-06-28 22:51:34 +0200

    Add command line args to BenchMark.java and ability to read in all files from a given directory

--- src/main/java/com/mbdr/benchmarking/BenchMark.java
+++ src/main/java/com/mbdr/benchmarking/BenchMark.java
@@ -6,12 +6,14 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.openjdk.jmh.infra.Blackhole;
-
+import org.tweetyproject.commons.ParserException;
 import org.tweetyproject.logics.pl.semantics.NicePossibleWorld;
 import org.tweetyproject.logics.pl.syntax.PlBeliefSet;
+import org.tweetyproject.logics.pl.syntax.PlFormula;
 import org.tweetyproject.logics.pl.syntax.PlSignature;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -24,35 +26,67 @@ import com.mbdr.utils.parsing.*;
 import com.mbdr.structures.*;
 
 import com.mbdr.formulabased.BaseRank;
+import com.mbdr.modelbased.EntailmentChecker;
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class BenchMark {
 
-    public final static String KNOWLEDGE_BASE_DIR = "knowledge_bases/Joel/ranks10/";
-
     @State(Scope.Benchmark) // all threads running the benchmark share the same state object.
     public static class StateObj {
 
+        @Param({ "knowledge_bases/" })
+        String kBDirectory;
+
+        @Param({ "testing.txt" })
+        String kBFileName;
+
+        @Param({ "testingQueries.txt" })
+        String queriesFileName;
+
+        @Param({ "modelbased" })
+        String benchmarkGroup;
+
         KnowledgeBase knowledgeBase;
+        ArrayList<PlBeliefSet> ranked_KB;
         Set<NicePossibleWorld> KB_U;
+        ArrayList<Set<NicePossibleWorld>> RC_Minimal_Model;
+        ArrayList<Set<NicePossibleWorld>> LC_Minimal_Model;
+        ArrayList<String> rawQueries;
 
         @Setup(Level.Trial) // Level.Trial is the default level. The method is called once for each full run
-                            // of the benchmark
+        // of the benchmark
         public void setup() {
             System.out.println("*****State initialization happening here****");
 
-            String fileName = "testing.txt";
-
-            System.out.println("reading in:\t" + fileName);
-            KnowledgeBaseReader reader = new KnowledgeBaseReader(KNOWLEDGE_BASE_DIR);
+            System.out.println("reading in:\t" + kBFileName);
+            KnowledgeBaseReader reader = new KnowledgeBaseReader(kBDirectory);
 
             try {
-                ArrayList<String> rawFormulas = reader.readFormulasFromFile(fileName);
+                // Read in the knowledge base and store in state object
+                ArrayList<String> rawFormulas = reader.readFormulasFromFile(kBFileName);
                 this.knowledgeBase = Parser.parseFormulas(rawFormulas);
+
+                // Rank the knowledge base using baserank - for benchmarking formulabased
+                // entailment checking
+                this.ranked_KB = BaseRank.BaseRankDirectImplementation(this.knowledgeBase);
+
+                // Enumerate all possible worlds w.r.t. atoms of KB to reduce benchmarking
+                // runtime across repeated runs
                 PlSignature KB_atoms = this.knowledgeBase.union().getMinimalSignature();
                 System.out.print("Getting all possible worlds w.r.t. atoms:\t" + KB_atoms);
                 this.KB_U = NicePossibleWorld.getAllPossibleWorlds(KB_atoms.toCollection());
+
+                // Construct the ranked models for RC and LC for query benchmarking
+                this.RC_Minimal_Model = com.mbdr.modelbased.RationalClosure
+                        .ConstructRankedModel(knowledgeBase, this.KB_U);
+                this.LC_Minimal_Model = com.mbdr.modelbased.LexicographicClosure
+                        .refine(knowledgeBase, RC_Minimal_Model);
+
+                System.out.println("reading in:\t" + queriesFileName);
+                // Read in all the queries from the query file
+                this.rawQueries = reader.readFormulasFromFile(queriesFileName);
+
             } catch (Exception e) {
                 // TODO: handle exception
             }
@@ -74,29 +108,52 @@ public class BenchMark {
      */
 
     @Benchmark
-    @Fork(value = 2) // 2 trails in total
+    @Fork(value = 2) // 2 trials in total
     @Measurement(iterations = 10, time = 1) // 10 iterations
     @Warmup(iterations = 5, time = 1) // 5 iterations of warmup
-    public void baseRankDirectImplementation(StateObj stateObj, Blackhole blackhole) throws InterruptedException {
+    public void formulabased_baserank_direct_implementation(StateObj stateObj, Blackhole blackhole)
+            throws InterruptedException {
         ArrayList<PlBeliefSet> ranked_KB = BaseRank.BaseRankDirectImplementation(stateObj.knowledgeBase);
         blackhole.consume(ranked_KB);
     }
 
     @Benchmark
-    @Fork(value = 2) // 2 trails in total
+    @Fork(value = 2) // 2 trials in total
     @Measurement(iterations = 10, time = 1) // 10 iterations
     @Warmup(iterations = 5, time = 1) // 5 iterations of warmup
-    public void constructRankedModelRC(StateObj stateObj, Blackhole blackhole) throws InterruptedException {
+    public void formulabased_RC_Joel_Regular(StateObj stateObj, Blackhole blackhole) throws InterruptedException {
+
+        for (String rawQuery : stateObj.rawQueries) {
+            try {
+                boolean queryAnswer = com.mbdr.formulabased.RationalClosure.RationalClosureJoelRegular(
+                        stateObj.ranked_KB,
+                        rawQuery);
+                blackhole.consume(queryAnswer);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    @Benchmark
+    @Fork(value = 2) // 2 trials in total
+    @Measurement(iterations = 10, time = 1) // 10 iterations
+    @Warmup(iterations = 5, time = 1) // 5 iterations of warmup
+    public void modelbased_construct_ranked_model_RC(StateObj stateObj, Blackhole blackhole)
+            throws InterruptedException {
         ArrayList<Set<NicePossibleWorld>> RC_Minimal_Model = com.mbdr.modelbased.RationalClosure
                 .ConstructRankedModel(stateObj.knowledgeBase, stateObj.KB_U);
         blackhole.consume(RC_Minimal_Model); // consume to avoid dead code elimination just in case?
     }
 
     @Benchmark
-    @Fork(value = 2) // 2 trails in total
+    @Fork(value = 2) // 2 trials in total
     @Measurement(iterations = 10, time = 1) // 10 iterations
     @Warmup(iterations = 5, time = 1) // 5 iterations of warmup
-    public void constructRankedModelLC(StateObj stateObj, Blackhole blackhole) throws InterruptedException {
+    public void modelbased_construct_ranked_model_LC(StateObj stateObj, Blackhole blackhole)
+            throws InterruptedException {
         ArrayList<Set<NicePossibleWorld>> RC_Minimal_Model = com.mbdr.modelbased.RationalClosure
                 .ConstructRankedModel(stateObj.knowledgeBase, stateObj.KB_U);
         ArrayList<Set<NicePossibleWorld>> LC_Minimal_Model = com.mbdr.modelbased.LexicographicClosure
@@ -104,25 +161,86 @@ public class BenchMark {
         blackhole.consume(LC_Minimal_Model); // consume to avoid dead code elimination just in case?
     }
 
+    @Benchmark
+    @Fork(value = 2) // 2 trials in total
+    @Measurement(iterations = 10, time = 1) // 10 iterations
+    @Warmup(iterations = 5, time = 1) // 5 iterations of warmup
+    public void modelbased_entailment_checker_RC(StateObj stateObj, Blackhole blackhole) throws InterruptedException {
+
+        EntailmentChecker rcChecker = new EntailmentChecker(stateObj.RC_Minimal_Model);
+
+        for (String rawQuery : stateObj.rawQueries) {
+            try {
+                boolean queryAnswer = rcChecker.query(rawQuery);
+                blackhole.consume(queryAnswer);
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
+
+    }
+
+    @Benchmark
+    @Fork(value = 2) // 2 trials in total
+    @Measurement(iterations = 10, time = 1) // 10 iterations
+    @Warmup(iterations = 5, time = 1) // 5 iterations of warmup
+    public void modelbased_entailment_checker_LC(StateObj stateObj, Blackhole blackhole) throws InterruptedException {
+
+        EntailmentChecker rcChecker = new EntailmentChecker(stateObj.LC_Minimal_Model);
+
+        for (String rawQuery : stateObj.rawQueries) {
+            try {
+                boolean queryAnswer = rcChecker.query(rawQuery);
+                blackhole.consume(queryAnswer);
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
+
+    }
+
     public static void main(String[] args) throws RunnerException {
 
-        // ArrayList<String> fileNames = new ArrayList<>();
-        // File folder = new File(KNOWLEDGE_BASE_DIR);
-        // File[] listOfFiles = folder.listFiles();
+        // TODO: Integrate full CLI interface using scaffold of BenchMarkCLI
 
-        // for (File file : listOfFiles) {
-        // if (file.isFile()) {
-        // // System.out.println(file.getName());
-        // fileNames.add(file.getName());
-        // }
-        // }
+        // Expected arguments:
+        // args[0] = directory where knowledge base textfiles are located
+        // args[1] = flag to indicate which set of benchmarks to run (currently two
+        // flags: "modelbased" or "formulabased")
+
+        System.out.println("Args:");
+        for (String string : args) {
+            System.out.println(string);
+        }
+
+        System.out.println("-----------------------------------------");
+        System.out.println("Found files:");
+        System.out.println("-----------------------------------------");
+        ArrayList<String> fileNames = new ArrayList<>();
+        File folder = new File(args[0]);
+        File[] listOfFiles = folder.listFiles();
+
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                System.out.println(file.getName());
+                fileNames.add(file.getName());
+            }
+        }
+        System.out.println("-----------------------------------------");
+        System.out.println("Running benchmark harness...");
+        System.out.println("-----------------------------------------");
 
         Options opt = new OptionsBuilder()
-                .include(BenchMark.class.getSimpleName())
+                // .include(BenchMark.class.getSimpleName())
+                .include("^com.mbdr.benchmarking.BenchMark." + args[1] + ".*")
                 .forks(1)
+                .param("kBDirectory", args[0])
+                .param("kBFileName", fileNames.toArray(new String[0]))
                 .build();
 
+        // new Runner(opt).list();
         new Runner(opt).run();
+
     }
 
 }
