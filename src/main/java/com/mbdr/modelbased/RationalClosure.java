d773271576ad705002f3c9cd65e314f21ecaac40
Jaron Cohen <jdicohen@gmail.com>
2022-07-04 16:08:57 +0200

    Add implementation of an algorithm to construct the Rational Closure ranked model using BaseRank

--- src/main/java/com/mbdr/modelbased/RationalClosure.java
+++ src/main/java/com/mbdr/modelbased/RationalClosure.java
@@ -16,6 +16,7 @@ import org.tweetyproject.logics.pl.semantics.NicePossibleWorld;
 
 import org.tweetyproject.logics.pl.sat.Sat4jSolver;
 import org.tweetyproject.logics.pl.sat.SatSolver;
+import org.tweetyproject.commons.util.Pair;
 import org.tweetyproject.logics.pl.reasoner.*;
 
 public class RationalClosure {
@@ -148,6 +149,165 @@ public class RationalClosure {
         RankedModel.add(infiniteRank);
 
         return RankedModel;
+    }
+
+    /**
+     * Constructs the ranked model used to define Rational Closure for a given
+     * knowledge base using BaseRank to facilitate its construction
+     * 
+     * @param knowledge
+     * @param possibleWorlds
+     * @return
+     */
+    public static ArrayList<Set<NicePossibleWorld>> ConstructRankedModelBaseRank(KnowledgeBase knowledge,
+            Set<NicePossibleWorld> possibleWorlds) {
+
+        // PlBeliefSet kBunion = knowledge.union();
+        // System.out.println("knowledgeBase union:\t" + kBunion);
+        // PlSignature kBsignature = kBunion.getMinimalSignature();
+        // System.out.println("knowledgeBase signature:\t" + kBsignature);
+
+        // Set<NicePossibleWorld> kB_PossibleWorlds = NicePossibleWorld
+        // .getAllPossibleWorlds(kBsignature.toCollection());
+        // System.out.println("kB_PossibleWorlds:\t" + kB_PossibleWorlds);
+
+        Set<NicePossibleWorld> infinite_worlds = new HashSet<>();
+
+        for (NicePossibleWorld pWorld : possibleWorlds) {
+            if (!pWorld.satisfies(knowledge.getPropositionalKnowledge())) {
+                infinite_worlds.add(pWorld);
+            }
+        }
+
+        Set<NicePossibleWorld> finite_worlds = new HashSet<>(possibleWorlds);
+        finite_worlds.removeAll(infinite_worlds);
+
+        // System.out.println("infinite_worlds:\t" + infinite_worlds);
+        // System.out.println("finite_worlds:\t\t" + finite_worlds);
+
+        // Now we need to rank the finite worlds using the base rank information hence
+        // we
+        // apply BaseRank to the knowledge base
+        ArrayList<PlBeliefSet> ranked_KB = com.mbdr.formulabased.BaseRank.BaseRankDirectImplementation(knowledge);
+
+        // HashMap of finite knowledge base formulas to their base rank
+        HashMap<PlFormula, Integer> kB_BR_HashMap = new HashMap<>();
+
+        for (int i = 0; i < ranked_KB.size() - 1; i++) {
+            for (PlFormula formula : ranked_KB.get(i)) {
+                kB_BR_HashMap.put(formula, i);
+            }
+        }
+        // System.out.println(kB_BR_HashMap);
+
+        // Map every finite rank world to the formula with the max base rank whose
+        // antecedent they satisfy
+        HashMap<NicePossibleWorld, Pair<PlFormula, Integer>> world_max_rank_map = new HashMap<>();
+
+        for (NicePossibleWorld finiteWorld : finite_worlds) {
+            // System.out.println("Current finite world:\t" + finiteWorld);
+            world_max_rank_map.put(finiteWorld, null);
+            int tempMaxRank = 0;
+            PlFormula tempMaxFormula = null;
+            for (PlFormula formula : knowledge.getDefeasibleKnowledge()) {
+                PlFormula antecedent = ((Implication) formula).getFirstFormula();
+                if (finiteWorld.satisfies(antecedent) && kB_BR_HashMap.get(formula) >= tempMaxRank) {
+                    tempMaxRank = kB_BR_HashMap.get(formula);
+                    tempMaxFormula = formula;
+                }
+            }
+
+            world_max_rank_map.put(finiteWorld, new Pair<PlFormula, Integer>(tempMaxFormula, tempMaxRank));
+
+        }
+
+        ArrayList<Set<NicePossibleWorld>> preliminary_ranked_model = new ArrayList<>();
+
+        // Initialise the ranked model to have the minimum number of ranks - i.e. as
+        // many ranks as there are base ranks
+        for (int i = 0; i < ranked_KB.size(); i++) {
+            preliminary_ranked_model.add(new HashSet<NicePossibleWorld>());
+        }
+
+        world_max_rank_map.entrySet().forEach(entry -> {
+
+            // Store the max base rank value of the current entry
+            int rankIndex = entry.getValue().getSecond();
+            // System.out.println("rankIndex:\t" + rankIndex);
+
+            Set<NicePossibleWorld> rank = preliminary_ranked_model.get(rankIndex);
+            rank.add(entry.getKey());
+
+        });
+
+        // System.out.println("----------------------------");
+        // System.out.println("Preliminary Ranked Model:");
+        // System.out.println("----------------------------");
+
+        // System.out.println(preliminary_ranked_model);
+
+        // System.out.println("----------------------------");
+
+        // System.out.println("∞" + " :\t" + infinite_worlds);
+        // for (int rank_Index = preliminary_ranked_model.size() - 1; rank_Index >= 0;
+        // rank_Index--) {
+        // System.out.println(rank_Index + " :\t" +
+        // preliminary_ranked_model.get(rank_Index));
+        // }
+
+        for (int rankIndex = 0; rankIndex < preliminary_ranked_model.size(); rankIndex++) {
+            for (NicePossibleWorld world : preliminary_ranked_model.get(rankIndex)) {
+                if (!world.satisfies(ranked_KB.get(rankIndex))) {
+                    int previousIndex = world_max_rank_map.get(world).getSecond();
+                    world_max_rank_map.get(world).setSecond(previousIndex + 1);
+                }
+            }
+        }
+
+        ArrayList<Set<NicePossibleWorld>> updated_ranked_model = new ArrayList<>();
+
+        // Initialise the ranked model to have the minimum number of ranks - i.e. as
+        // many ranks as there are base ranks
+        for (int i = 0; i < ranked_KB.size(); i++) {
+            updated_ranked_model.add(new HashSet<NicePossibleWorld>());
+        }
+
+        world_max_rank_map.entrySet().forEach(entry -> {
+
+            // Store the max base rank value of the current entry
+            int rankIndex = entry.getValue().getSecond();
+
+            if (rankIndex >= updated_ranked_model.size()) {
+                Set<NicePossibleWorld> newRank = new HashSet<>();
+                newRank.add(entry.getKey());
+                updated_ranked_model.add(rankIndex, newRank); // Add rank at correct rank index
+
+            } else {
+                Set<NicePossibleWorld> rank = updated_ranked_model.get(rankIndex);
+                rank.add(entry.getKey());
+            }
+
+        });
+
+        // System.out.println("----------------------------");
+        // System.out.println("Updated Ranked Model:");
+        // System.out.println("----------------------------");
+
+        // System.out.println(updated_ranked_model);
+
+        // System.out.println("----------------------------");
+
+        // System.out.println("∞" + " :\t" + infinite_worlds);
+        // for (int rank_Index = updated_ranked_model.size() - 1; rank_Index >= 0;
+        // rank_Index--) {
+        // System.out.println(rank_Index + " :\t" +
+        // updated_ranked_model.get(rank_Index));
+        // }
+
+        updated_ranked_model.add(infinite_worlds);
+
+        return updated_ranked_model;
+
     }
 
 }
