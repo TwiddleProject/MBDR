b786e68c1dac6f1e8f175ee84fe57fe6647b9179
Carl Combrinck <carljcombrinck@gmail.com>
2022-07-13 01:35:26 +0200

    Rename and implement RankConstructor interface

--- src/main/java/com/mbdr/formulabased/BaseRankConstructor.java
+++ src/main/java/com/mbdr/formulabased/BaseRankConstructor.java
@@ -7,13 +7,14 @@ import org.tweetyproject.logics.pl.syntax.Negation;
 import org.tweetyproject.logics.pl.syntax.PlBeliefSet;
 import org.tweetyproject.logics.pl.syntax.PlFormula;
 
+import com.mbdr.services.RankConstructor;
 import com.mbdr.structures.DefeasibleKnowledgeBase;
 
 import org.tweetyproject.logics.pl.sat.Sat4jSolver;
 import org.tweetyproject.logics.pl.sat.SatSolver;
 import org.tweetyproject.logics.pl.reasoner.*;
 
-public class BaseRank {
+public class BaseRankConstructor implements RankConstructor<ArrayList<PlBeliefSet>>{
 
     /**
      * Standard, unoptimised BaseRank algorithm implementation
@@ -22,7 +23,7 @@ public class BaseRank {
      * @param KB_D
      * @return
      */
-    public static ArrayList<PlBeliefSet> BaseRankDirectImplementation(DefeasibleKnowledgeBase knowledge) {
+    public ArrayList<PlBeliefSet> construct(DefeasibleKnowledgeBase knowledge) {
         SatSolver.setDefaultSolver(new Sat4jSolver());
         SatReasoner reasoner = new SatReasoner();
         ArrayList<PlBeliefSet> rankedKB = new ArrayList<PlBeliefSet>();
@@ -70,59 +71,6 @@ public class BaseRank {
 
         return rankedKB;
 
-    }
-
-    /**
-     * NB this needs to be tested - not sure if producing correct results
-     * 
-     * @param kb
-     * @param classicalStatements
-     * @return
-     */
-    static ArrayList<PlBeliefSet> BaseRankJoel(PlBeliefSet kb, PlBeliefSet classicalStatements) {
-        SatSolver.setDefaultSolver(new Sat4jSolver());
-        SatReasoner reasoner = new SatReasoner();
-        ArrayList<PlBeliefSet> rankedKB = new ArrayList<PlBeliefSet>();
-        PlBeliefSet currentMaterialisation = kb;
-        PlBeliefSet prevMaterialisation = new PlBeliefSet();
-
-        while (!currentMaterialisation.equals(prevMaterialisation)) {
-            prevMaterialisation = currentMaterialisation;
-            // System.out.println("prevMaterialisation: \t" + prevMaterialisation);
-            currentMaterialisation = new PlBeliefSet();
-            for (PlFormula f : prevMaterialisation) {
-                if (f.toString().contains("=>")) {
-                    if (reasoner.query(prevMaterialisation, new Negation(((Implication) f).getFormulas().getFirst()))) {
-                        currentMaterialisation.add(f);
-                    }
-                }
-            }
-            // System.out.println("currentMaterialisation:\t" + currentMaterialisation);
-            PlBeliefSet newRank = new PlBeliefSet();
-            for (PlFormula form : prevMaterialisation) {
-                if (!classicalStatements.contains(form)) {
-                    newRank.add(form);
-                }
-            }
-            newRank.removeAll(currentMaterialisation);
-            // System.out.println("newRank:\t" + newRank);
-            if (newRank.size() != 0) {
-                rankedKB.add(newRank);
-                System.out.println("Added rank " + Integer.toString(rankedKB.size() - 1));
-            } else {
-                classicalStatements.addAll(currentMaterialisation);
-            }
-        }
-        rankedKB.add(classicalStatements);
-        System.out.println("Base Ranking of Knowledge Base:");
-        for (PlBeliefSet rank : rankedKB) {
-            if (rankedKB.indexOf(rank) == rankedKB.size() - 1) {
-                System.out.println("Infinite Rank:" + rank.toString());
-            } else {
-                System.out.println("Rank " + Integer.toString(rankedKB.indexOf(rank)) + ":" + rank.toString());
-            }
-        }
-        return rankedKB;
     }
 
 }
