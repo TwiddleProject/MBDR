2586244b28bfdd4d231987889b329ab7356406cd
Carl Combrinck <carljcombrinck@gmail.com>
2022-07-13 01:33:51 +0200

    Add default query implementation and build method

--- src/main/java/com/mbdr/services/DefeasibleQueryChecker.java
+++ src/main/java/com/mbdr/services/DefeasibleQueryChecker.java
@@ -3,10 +3,42 @@ package com.mbdr.services;
 import org.tweetyproject.logics.pl.syntax.Implication;
 import org.tweetyproject.logics.pl.syntax.PlFormula;
 
+import com.mbdr.structures.DefeasibleKnowledgeBase;
+import com.mbdr.utils.parsing.Parser;
+
 public interface DefeasibleQueryChecker {
     
-    public boolean query(String formula);
-    public boolean queryPropositional(PlFormula formula);
-    public boolean queryDefeasible(Implication defeasibleImplication);
+    default boolean query(String formula){
+        if(Parser.isDefeasible(formula)){
+            try{
+                Implication defeasibleImplication = (Implication)Parser.parseDefeasibleFormula(formula);
+                return queryDefeasible(defeasibleImplication);
+            } catch(Exception e){
+                throw new InvalidFormula("Invalid formula for defeasible query.");
+            }
+        } 
+        else {
+            try{
+                PlFormula propositionalFormula = Parser.parsePropositionalFormula(formula);
+                return queryPropositional(propositionalFormula);
+            } catch(Exception e){
+                throw new InvalidFormula("Invalid formula for propositional query.");
+            }
+        }
+    }
 
+    boolean queryPropositional(PlFormula formula);
+    boolean queryDefeasible(Implication defeasibleImplication);
+    void build(DefeasibleKnowledgeBase knowledge);
+
+    public static class InvalidFormula extends RuntimeException {
+
+        public InvalidFormula(){
+            super();
+        }
+
+        public InvalidFormula(String message){
+            super(message);
+        }
+    }
 }
