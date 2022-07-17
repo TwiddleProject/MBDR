20f30d4392e1e62d4ebfccf327075c5b2b4f480a
Carl Combrinck <carljcombrinck@gmail.com>
2022-07-17 05:47:58 +0200

    Separate algorithms into RankConstructor classes

---
+++ src/main/java/com/mbdr/modelbased/RationalModelBaseRankConstructor.java
@@ -0,0 +1,104 @@
+package com.mbdr.modelbased;
+
+import java.util.*;
+
+import org.tweetyproject.logics.pl.syntax.Implication;
+import org.tweetyproject.logics.pl.syntax.PlBeliefSet;
+import org.tweetyproject.logics.pl.syntax.PlFormula;
+
+import com.mbdr.formulabased.BaseRankConstructor;
+import com.mbdr.services.RankConstructor;
+import com.mbdr.structures.DefeasibleKnowledgeBase;
+
+import org.tweetyproject.logics.pl.semantics.NicePossibleWorld;
+
+//TODO Refactor to be RankConstructor<RankedInterpretation>
+public class RationalModelBaseRankConstructor implements RankConstructor<ArrayList<Set<NicePossibleWorld>>>{
+
+
+    /**
+     * Constructs the ranked model used to define Rational Closure for a given
+     * knowledge base using BaseRank to facilitate its construction
+     * 
+     * @param knowledge
+     * @return
+     */
+    @Override
+    public ArrayList<Set<NicePossibleWorld>> construct(DefeasibleKnowledgeBase knowledge) {
+        
+        // Apply BaseRank to the knowledge base
+        ArrayList<PlBeliefSet> ranked_KB = new BaseRankConstructor().construct(knowledge);
+
+        // System.out.println("ranked_KB:\t" + ranked_KB);
+
+        // HashMap of finite knowledge base formulas to their base rank
+        HashMap<PlFormula, Integer> kB_BR_HashMap = new HashMap<>();
+
+        for (int i = 0; i < ranked_KB.size() - 1; i++) {
+            for (PlFormula formula : ranked_KB.get(i)) {
+                kB_BR_HashMap.put(formula, i);
+            }
+        }
+        // System.out.println("kB_BR_HashMap:\t" + kB_BR_HashMap);
+
+        // Set to contain all the infinite rank worlds
+        Set<NicePossibleWorld> infinite_worlds = new HashSet<>();
+
+        ArrayList<Set<NicePossibleWorld>> ranked_model = new ArrayList<>();
+
+        // Initialise the ranked model to have the minimum number of ranks - i.e. as
+        // many ranks as there are base ranks
+        for (int i = 0; i < ranked_KB.size(); i++) {
+            ranked_model.add(new HashSet<NicePossibleWorld>());
+        }
+
+        Set<NicePossibleWorld> kB_PossibleWorlds = NicePossibleWorld.getAllPossibleWorlds(
+            knowledge.union().getMinimalSignature().toCollection()
+        );
+
+        for (NicePossibleWorld pWorld : kB_PossibleWorlds) {
+
+            // If the current world does not satisfy the classical propositional formulas
+            // then add to infinite rank
+            if (!pWorld.satisfies(knowledge.getPropositionalKnowledge())) {
+                infinite_worlds.add(pWorld);
+            } else {
+                int tempMaxRank = 0;
+
+                // Find the maximum rank of a knowledge base formula for which the current
+                // world satisfies its antecedent
+                for (PlFormula formula : knowledge.getDefeasibleKnowledge()) {
+                    PlFormula antecedent = ((Implication) formula).getFirstFormula();
+                    if (pWorld.satisfies(antecedent) && kB_BR_HashMap.get(formula) >= tempMaxRank) {
+                        tempMaxRank = kB_BR_HashMap.get(formula);
+                    }
+                }
+
+                // If the current world does not satisfy all the formulas on its max rank then
+                // bump it up
+                if (!pWorld.satisfies(ranked_KB.get(tempMaxRank))) {
+                    tempMaxRank++;
+                }
+
+                // If the rank of the world exceeds the number of ranks that exist, then create
+                // a new rank, add the current world to it and add the rank to the ranked model
+                if (tempMaxRank >= ranked_model.size()) {
+                    Set<NicePossibleWorld> newRank = new HashSet<>();
+                    newRank.add(pWorld);
+                    ranked_model.add(tempMaxRank, newRank); // Add rank at correct rank index
+                } else {
+                    // Otherwise add the world to its correct rank in the ranked model
+                    Set<NicePossibleWorld> rank = ranked_model.get(tempMaxRank);
+                    rank.add(pWorld);
+                }
+            }
+        }
+
+        // Lastly add the infinite rank worlds
+        ranked_model.add(infinite_worlds);
+
+        return ranked_model;
+
+    }
+
+}
--- src/main/java/com/mbdr/modelbased/RationalModelConstructor.java
+++ src/main/java/com/mbdr/modelbased/RationalModelConstructor.java
@@ -6,14 +6,13 @@ import java.util.Map.Entry;
 import org.tweetyproject.logics.pl.syntax.Implication;
 import org.tweetyproject.logics.pl.syntax.PlBeliefSet;
 import org.tweetyproject.logics.pl.syntax.PlFormula;
-import org.tweetyproject.logics.pl.syntax.PlSignature;
 
-import com.mbdr.formulabased.BaseRankConstructor;
+import com.mbdr.services.RankConstructor;
 import com.mbdr.structures.DefeasibleKnowledgeBase;
 
 import org.tweetyproject.logics.pl.semantics.NicePossibleWorld;
 
-public class RationalClosure {
+public class RationalModelConstructor implements RankConstructor<ArrayList<Set<NicePossibleWorld>>>{
 
     /**
      * Constructs the ranked model used to define Rational Closure for a given
@@ -25,8 +24,8 @@ public class RationalClosure {
      *             base
      * @return
      */
-    public static ArrayList<Set<NicePossibleWorld>> ConstructRankedModel(DefeasibleKnowledgeBase knowledge,
-            Set<NicePossibleWorld> possibleWorlds) {
+    @Override
+    public ArrayList<Set<NicePossibleWorld>> construct(DefeasibleKnowledgeBase knowledge) {
         // TODO: Clean up more...
         // TODO: Add proper logger
         // TODO: Might want to change name of finite and infinite - maybe infinite and
@@ -37,16 +36,9 @@ public class RationalClosure {
         // System.out.println("Beginning construction of minimal ranked model for
         // RC...");
         // System.out.println("------------------------------------------------------------------------------------");
-        PlBeliefSet KB = knowledge.union();
-        PlSignature KB_atoms = KB.getMinimalSignature();
-        // System.out.println("Atoms:\t" + KB_atoms);
-
-        Set<NicePossibleWorld> KB_U;
-        if (possibleWorlds == null) {
-            KB_U = NicePossibleWorld.getAllPossibleWorlds(KB_atoms.toCollection());
-        } else {
-            KB_U = new HashSet<>(possibleWorlds);
-        }
+        Set<NicePossibleWorld> KB_U = NicePossibleWorld.getAllPossibleWorlds(
+            knowledge.union().getMinimalSignature().toCollection()
+        );
 
         // System.out.println("All Worlds:\t" + KB_U);
         // Find the worlds that do not satisfy the classical formulas - must have
@@ -143,88 +135,6 @@ public class RationalClosure {
         RankedModel.add(infiniteRank);
 
         return RankedModel;
-    }
-
-    /**
-     * Constructs the ranked model used to define Rational Closure for a given
-     * knowledge base using BaseRank to facilitate its construction
-     * 
-     * @param knowledge
-     * @param possibleWorlds
-     * @return
-     */
-    public static ArrayList<Set<NicePossibleWorld>> ConstructRankedModelBaseRank(DefeasibleKnowledgeBase knowledge,
-            Set<NicePossibleWorld> possibleWorlds) {
-
-        // Apply BaseRank to the knowledge base
-        ArrayList<PlBeliefSet> ranked_KB = new BaseRankConstructor().construct(knowledge);
-
-        // System.out.println("ranked_KB:\t" + ranked_KB);
-
-        // HashMap of finite knowledge base formulas to their base rank
-        HashMap<PlFormula, Integer> kB_BR_HashMap = new HashMap<>();
-
-        for (int i = 0; i < ranked_KB.size() - 1; i++) {
-            for (PlFormula formula : ranked_KB.get(i)) {
-                kB_BR_HashMap.put(formula, i);
-            }
-        }
-        // System.out.println("kB_BR_HashMap:\t" + kB_BR_HashMap);
-
-        // Set to contain all the infinite rank worlds
-        Set<NicePossibleWorld> infinite_worlds = new HashSet<>();
-
-        ArrayList<Set<NicePossibleWorld>> ranked_model = new ArrayList<>();
-
-        // Initialise the ranked model to have the minimum number of ranks - i.e. as
-        // many ranks as there are base ranks
-        for (int i = 0; i < ranked_KB.size(); i++) {
-            ranked_model.add(new HashSet<NicePossibleWorld>());
-        }
-
-        for (NicePossibleWorld pWorld : possibleWorlds) {
-
-            // If the current world does not satisfy the classical propositional formulas
-            // then add to infinite rank
-            if (!pWorld.satisfies(knowledge.getPropositionalKnowledge())) {
-                infinite_worlds.add(pWorld);
-            } else {
-                int tempMaxRank = 0;
-
-                // Find the maximum rank of a knowledge base formula for which the current
-                // world satisfies its antecedent
-                for (PlFormula formula : knowledge.getDefeasibleKnowledge()) {
-                    PlFormula antecedent = ((Implication) formula).getFirstFormula();
-                    if (pWorld.satisfies(antecedent) && kB_BR_HashMap.get(formula) >= tempMaxRank) {
-                        tempMaxRank = kB_BR_HashMap.get(formula);
-                    }
-                }
-
-                // If the current world does not satisfy all the formulas on its max rank then
-                // bump it up
-                if (!pWorld.satisfies(ranked_KB.get(tempMaxRank))) {
-                    tempMaxRank++;
-                }
-
-                // If the rank of the world exceeds the number of ranks that exist, then create
-                // a new rank, add the current world to it and add the rank to the ranked model
-                if (tempMaxRank >= ranked_model.size()) {
-                    Set<NicePossibleWorld> newRank = new HashSet<>();
-                    newRank.add(pWorld);
-                    ranked_model.add(tempMaxRank, newRank); // Add rank at correct rank index
-                } else {
-                    // Otherwise add the world to its correct rank in the ranked model
-                    Set<NicePossibleWorld> rank = ranked_model.get(tempMaxRank);
-                    rank.add(pWorld);
-                }
-            }
-        }
-
-        // Lastly add the infinite rank worlds
-        ranked_model.add(infinite_worlds);
-
-        return ranked_model;
-
     }
 
 }
