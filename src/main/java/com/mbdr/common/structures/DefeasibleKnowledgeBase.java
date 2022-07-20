10af11c2121670b9f2981382127c009bce822126
Carl Combrinck <carljcombrinck@gmail.com>
2022-07-20 22:07:12 +0200

    Create general collection class for formulas

--- src/main/java/com/mbdr/common/structures/DefeasibleKnowledgeBase.java
+++ src/main/java/com/mbdr/common/structures/DefeasibleKnowledgeBase.java
@@ -1,53 +1,18 @@
 package com.mbdr.common.structures;
 
 import org.tweetyproject.logics.pl.syntax.PlBeliefSet;
-import org.tweetyproject.logics.pl.syntax.PlFormula;
 
-public class DefeasibleKnowledgeBase {
-    
-    private PlBeliefSet defeasibleKnowledge;
-    private PlBeliefSet propositionalKnowledge;
+public class DefeasibleKnowledgeBase extends DefeasibleFormulaCollection{
 
     public DefeasibleKnowledgeBase(){
-        this.defeasibleKnowledge = new PlBeliefSet();
-        this.propositionalKnowledge = new PlBeliefSet();
+        super();
+    }
+
+    public DefeasibleKnowledgeBase(DefeasibleFormulaCollection collection){
+        super(collection.getDefeasibleKnowledge(), collection.getPropositionalKnowledge());
     }
 
     public DefeasibleKnowledgeBase(PlBeliefSet defeasibleKnowledge, PlBeliefSet propositionalKnowledge){
-        this.defeasibleKnowledge = new PlBeliefSet(defeasibleKnowledge);
-        this.propositionalKnowledge = new PlBeliefSet(propositionalKnowledge);
+        super(defeasibleKnowledge, propositionalKnowledge);
     }
-
-    public void addDefeasibleFormula(PlFormula formula){
-        this.defeasibleKnowledge.add(formula);
-    }
-
-    public void addPropositionalFormula(PlFormula formula){
-        this.propositionalKnowledge.add(formula);
-    }
-
-    public PlBeliefSet getDefeasibleKnowledge(){
-        return new PlBeliefSet(this.defeasibleKnowledge);
-    }
-
-    public PlBeliefSet getPropositionalKnowledge(){
-        return new PlBeliefSet(this.propositionalKnowledge);
-    }
-
-    public PlBeliefSet union(){
-        return union(this.propositionalKnowledge, this.defeasibleKnowledge);
-    }
-
-    public static PlBeliefSet union(PlBeliefSet a, PlBeliefSet b){
-        PlBeliefSet temp = new PlBeliefSet();
-        temp.addAll(a);
-        temp.addAll(b);
-        return temp;
-    }
-
-    public String toString(){
-        return  "=> : " + this.propositionalKnowledge + "\n" + 
-                "~> : " + this.defeasibleKnowledge;
-    }
-
 }
