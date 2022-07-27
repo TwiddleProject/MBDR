3971af0a00bab4d75ab8451f2213041e44161625
Jaron Cohen <jdicohen@gmail.com>
2022-07-27 20:14:25 +0200

    Modify RankedFormulasInterpretation constructor to be more consistent with RankedInterpretation class and add some comments

--- src/main/java/com/mbdr/modelbased/structures/RankedFormulasInterpretation.java
+++ src/main/java/com/mbdr/modelbased/structures/RankedFormulasInterpretation.java
@@ -15,20 +15,30 @@ public class RankedFormulasInterpretation {
 
     public RankedFormulasInterpretation(int ranks) {
         this.ranks = new ArrayList<>();
-        for (int index = 0; index < ranks; ++index) {
+        for (int index = 0; index <= ranks; ++index) {
             this.ranks.add(null);
         }
     }
 
     public RankedFormulasInterpretation(ArrayList<PlFormula> ranks) {
-        this.ranks = ranks;
+        this.ranks = new ArrayList<>(ranks);
     }
 
+    /**
+     * Get the number of finite ranks
+     * 
+     * @return
+     */
     public int getRankCount() {
         return this.ranks.size() - 1;
     }
 
-    // get rankFormula from finite rank
+    /**
+     * Get the formula on the specified finite rank index
+     * 
+     * @param index - the finite rank index
+     * @return
+     */
     public PlFormula getRank(int index) {
         if (index >= this.ranks.size() - 1 || index < 0) {
             throw new RankOutOfBounds("Rank " + index + " does not exist.");
@@ -36,6 +46,12 @@ public class RankedFormulasInterpretation {
         return this.ranks.get(index);
     }
 
+    /**
+     * Set the formula for the specified finite rank index
+     * 
+     * @param index       - the finite rank index
+     * @param rankFormula - the rank formula
+     */
     public void setRank(int index, PlFormula rankFormula) {
         if (index >= this.ranks.size() - 1 || index < 0) {
             throw new RankOutOfBounds("Rank " + index + " is out of bounds.");
@@ -43,28 +59,53 @@ public class RankedFormulasInterpretation {
         this.ranks.set(index, rankFormula);
     }
 
+    /**
+     * Get the formula on the infinite rank
+     * 
+     * @return
+     */
     public PlFormula getInfiniteRank() {
         return this.ranks.get(this.ranks.size() - 1);
     }
 
+    /**
+     * Set the formula for the infinite rank
+     * 
+     * @param rankFormula
+     */
     public void setInfiniteRank(PlFormula rankFormula) {
         this.ranks.set(this.ranks.size() - 1, rankFormula);
     }
 
-    // Add an empty (null) new finite rank below the infinite rank
+    /**
+     * Add a new empty (null) finite rank below the infinite rank
+     * 
+     * @return
+     */
     public int addRank() {
         int position = this.getRankCount();
         this.ranks.add(position, null);
         return position;
     }
 
+    /**
+     * Add a new finite rank below the infinite rank containing the specified
+     * formula
+     * 
+     * @param rankFormula
+     * @return
+     */
     public int addRank(PlFormula rankFormula) {
         int position = this.getRankCount();
         this.ranks.add(position, rankFormula);
         return position;
     }
 
-    // Add a new empty (null) finite rank at specified index
+    /**
+     * Add a new empty (null) finite rank at the specified index
+     * 
+     * @param index
+     */
     public void addRank(int index) {
         if (index >= this.ranks.size() - 1 || index < 0) {
             throw new RankOutOfBounds("Rank " + index + " is out of bounds.");
@@ -72,6 +113,12 @@ public class RankedFormulasInterpretation {
         this.ranks.add(index, null);
     }
 
+    /**
+     * Add new rank containing the specified formula at the specified index
+     * 
+     * @param index
+     * @param rankFormula
+     */
     public void addRank(int index, PlFormula rankFormula) {
         if (index >= this.ranks.size() - 1 || index < 0) {
             throw new RankOutOfBounds("Rank " + index + " is out of bounds.");
