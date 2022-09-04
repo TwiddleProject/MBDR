5fdd5ae633c3a662c7afb909d03e42af2c5b7f53
Jaron Cohen <51699163+jaronsgit@users.noreply.github.com>
2022-09-04 16:54:29 +0200

    Fix bug in KBGenerationScript

--- scripts/knowledge_base_generation/KBGenerationScript.sh
+++ scripts/knowledge_base_generation/KBGenerationScript.sh
@@ -4,7 +4,7 @@ for ((i = 10 ; i <= 100 ; i+=10)); do
     # echo "Loop iteration: $i"
     for ((j = 1 ; j <= 10 ; j++)); do
         scala kbgt-assembly-1.0.jar -r $i -s $(($i*$j)) -d uniform -o "../../data/benchmarking/cumulative/${i}x${j}" --scadrFile  --defeasibleOnly --conservative
-        sed -i "" 's/~>/|~/g' "../../data/benchmarking/cumulative/ranks${i}x${j}.txt"
+        sed -i "" 's/~>/|~/g' "../../data/benchmarking/cumulative/${i}x${j}.txt"
         # echo "${i}x${j}"
     done
 done
