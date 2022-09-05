ffd009278f3e73c1b04ede03931de1d80f9ab098
Jaron Cohen <51699163+jaronsgit@users.noreply.github.com>
2022-09-06 01:20:55 +0200

    Update graph_construction.py

--- scripts/graphing/graph_construction.py
+++ scripts/graphing/graph_construction.py
@@ -7,27 +7,32 @@ import matplotlib.pyplot as plt
 FILE = "results/construction.csv"
 DATA_DIR = 'results/'
 
+
 def process_data(filename, input_file_param_name, constructor_param_name):
     raw = pd.read_csv(os.path.join(DATA_DIR, filename))
     # Split text file name into values
-    split_ranks = raw[input_file_param_name].str.replace(".txt", "").str.split("(?<=\d)x(?=\d)")
-    split_df = pd.DataFrame(split_ranks.tolist(), columns=['rank', 'rank_size'])
+    split_ranks = raw[input_file_param_name].str.replace(
+        ".txt", "").str.split("(?<=\d)x(?=\d)")
+    split_df = pd.DataFrame(split_ranks.tolist(),
+                            columns=['rank', 'rank_size'])
     df = pd.concat([raw, split_df], axis=1)
-    df['Score'] = df['Score'].str.replace(",", ".")
-    df.rename(columns = {constructor_param_name:'Algorithm'}, inplace=True)
-    df[['rank', 'rank_size', 'Score']] = df[['rank', 'rank_size', 'Score']].apply(pd.to_numeric)
+    # df['Score'] = df['Score'].str.replace(",", ".")
+    df.rename(columns={constructor_param_name: 'Algorithm'}, inplace=True)
+    df[['rank', 'rank_size', 'Score']] = df[[
+        'rank', 'rank_size', 'Score']].apply(pd.to_numeric)
     df['formulas'] = df['rank'] * df['rank_size']
     return df
 
-def plot_data(data_frame, output_file, x,y, title, xlab, ylab, legend=None):
+
+def plot_data(data_frame, output_file, x, y, title, xlab, ylab, legend=None):
     print("Plotting data...")
-    plt.figure(dpi = 300) 
+    plt.figure(dpi=300)
     with plt.style.context(['science', 'std-colors']):
         # Plot lines for each algorithm
         res = sns.lineplot(
-            x=x, 
-            y=y, 
-            hue='Algorithm', 
+            x=x,
+            y=y,
+            hue='Algorithm',
             data=data_frame
         )
         # Set labels
@@ -41,6 +46,7 @@ def plot_data(data_frame, output_file, x,y, title, xlab, ylab, legend=None):
         graph_dir = os.path.join(DATA_DIR, 'graphs')
         os.makedirs(graph_dir, exist_ok=True)
         plt.savefig(os.path.join(graph_dir, output_file))
+
 
 def main():
     data_frame = process_data(
@@ -76,6 +82,6 @@ def main():
         output_file="rank_size.png"
     )
 
+
 if __name__ == '__main__':
     main()
-
