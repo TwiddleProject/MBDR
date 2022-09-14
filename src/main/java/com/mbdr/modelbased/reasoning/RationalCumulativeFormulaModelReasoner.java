973429da8113c2510a4c1b6785429b503b8b44d8
Jaron Cohen <51699163+jaronsgit@users.noreply.github.com>
2022-09-14 15:12:32 +0200

    Update RationalCumulativeFormulaModelReasoner.java

--- src/main/java/com/mbdr/modelbased/reasoning/RationalCumulativeFormulaModelReasoner.java
+++ src/main/java/com/mbdr/modelbased/reasoning/RationalCumulativeFormulaModelReasoner.java
@@ -2,6 +2,11 @@ package com.mbdr.modelbased.reasoning;
 
 import com.mbdr.modelbased.construction.CumulativeFormulaRank;
 
+/**
+ * Reasoner wrapper class used mainly for efficient benchmarking purposes.
+ * Functions as a wrapper for the minimal ranked entailment formula reasoner that uses ranked formula models produced by
+ * CumulativeFormulaRank.
+ */
 public class RationalCumulativeFormulaModelReasoner extends MinimalRankedEntailmentCumulativeFormulaReasoner {
 
     public RationalCumulativeFormulaModelReasoner() {
