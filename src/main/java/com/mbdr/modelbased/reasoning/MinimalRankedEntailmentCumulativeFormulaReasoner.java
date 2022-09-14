c6f80c56cfc86ebc737afe67aadd2f77c80ecaff
Jaron Cohen <51699163+jaronsgit@users.noreply.github.com>
2022-09-14 14:47:25 +0200

    Update MinimalRankedEntailmentCumulativeFormulaReasoner.java

--- src/main/java/com/mbdr/modelbased/reasoning/MinimalRankedEntailmentCumulativeFormulaReasoner.java
+++ src/main/java/com/mbdr/modelbased/reasoning/MinimalRankedEntailmentCumulativeFormulaReasoner.java
@@ -3,12 +3,23 @@ package com.mbdr.modelbased.reasoning;
 import com.mbdr.common.services.RankConstructor;
 import com.mbdr.modelbased.structures.RankedFormulasInterpretation;
 
+/**
+ *
+ */
 public class MinimalRankedEntailmentCumulativeFormulaReasoner extends MinimalRankedEntailmentFormulaReasoner {
 
+    /**
+     *
+     * @param model
+     */
     public MinimalRankedEntailmentCumulativeFormulaReasoner(RankedFormulasInterpretation model) {
         super(model);
     }
 
+    /**
+     *
+     * @param constructor
+     */
     public MinimalRankedEntailmentCumulativeFormulaReasoner(RankConstructor<RankedFormulasInterpretation> constructor) {
         super(constructor);
     }
