ca6e05ce8a7bd7d7fc0f72ee0e7fd1073dbe4a7c
Carl Combrinck <carljcombrinck@gmail.com>
2022-09-14 21:20:52 +0200

    Document structure classes

--- src/main/java/com/mbdr/common/structures/DefeasibleQuerySet.java
+++ src/main/java/com/mbdr/common/structures/DefeasibleQuerySet.java
@@ -2,16 +2,33 @@ package com.mbdr.common.structures;
 
 import org.tweetyproject.logics.pl.syntax.PlBeliefSet;
 
+/**
+ * Implementation of a defeasible query set
+ */
 public class DefeasibleQuerySet extends DefeasibleFormulaCollection{
 
+    /**
+     * Default constructor
+     */
     public DefeasibleQuerySet(){
         super();
     }
 
+    /**
+     * Parameterized constructor
+     * 
+     * @param collection The collection of formulas
+     */
     public DefeasibleQuerySet(DefeasibleFormulaCollection collection){
         super(collection.getDefeasibleKnowledge(), collection.getPropositionalKnowledge());
     }
 
+    /**
+     * Parameterized constructor
+     * 
+     * @param defeasibleKnowledge The defeasible formulas
+     * @param propositionalKnowledge The propositional formulas
+     */
     public DefeasibleQuerySet(PlBeliefSet defeasibleKnowledge, PlBeliefSet propositionalKnowledge){
         super(defeasibleKnowledge, propositionalKnowledge);
     }
