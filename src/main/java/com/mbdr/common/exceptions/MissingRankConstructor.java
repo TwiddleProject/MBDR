b5bda4339b66fca17ebbd3e529db48677a883013
Carl Combrinck <carljcombrinck@gmail.com>
2022-09-14 17:11:38 +0200

    Add comments to common exceptions

--- src/main/java/com/mbdr/common/exceptions/MissingRankConstructor.java
+++ src/main/java/com/mbdr/common/exceptions/MissingRankConstructor.java
@@ -1,10 +1,22 @@
 package com.mbdr.common.exceptions;
 
+/**
+ * Exception for missing rank constructor for reasoner
+ */
 public class MissingRankConstructor extends RuntimeException{
+
+    /**
+     * Default constructor
+     */
     public MissingRankConstructor(){
         super();
     }
 
+    /**
+     * Parameterized constructor
+     * 
+     * @param message The error message
+     */
     public MissingRankConstructor(String message){
         super(message);
     }
