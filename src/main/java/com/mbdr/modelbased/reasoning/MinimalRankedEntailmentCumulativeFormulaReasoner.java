7ff4668512bcb1215ce3f7d99c356d1a7cf9c489
Jaron Cohen <51699163+jaronsgit@users.noreply.github.com>
2022-09-14 15:11:59 +0200

    Update MinimalRankedEntailmentCumulativeFormulaReasoner.java

--- src/main/java/com/mbdr/modelbased/reasoning/MinimalRankedEntailmentCumulativeFormulaReasoner.java
+++ src/main/java/com/mbdr/modelbased/reasoning/MinimalRankedEntailmentCumulativeFormulaReasoner.java
@@ -4,12 +4,15 @@ import com.mbdr.common.services.RankConstructor;
 import com.mbdr.modelbased.structures.RankedFormulasInterpretation;
 
 /**
- *
+ * This class provides a reasoner capable of answering defeasible entailment queries using the cumulative ranked formula
+ * model produced by CumulativeFormulaRank.
+ * Currently, this class is functionally equivalent to MinimalRankedEntailmentFormulaReasoner due to the semantics of
+ * answering entailment queries with cumulative ranked models.
  */
 public class MinimalRankedEntailmentCumulativeFormulaReasoner extends MinimalRankedEntailmentFormulaReasoner {
 
     /**
-     *
+     * Constructor to produce reasoner with a pre-existing RankedFormulasInterpretation model
      * @param model
      */
     public MinimalRankedEntailmentCumulativeFormulaReasoner(RankedFormulasInterpretation model) {
@@ -17,8 +20,9 @@ public class MinimalRankedEntailmentCumulativeFormulaReasoner extends MinimalRan
     }
 
     /**
-     *
-     * @param constructor
+     * Constructor to produce reasoner with a RankConstructor capable of generating the necessary RankedFormulasInterpretation
+     * model needed for reasoning
+     * @param constructor - RankConstructor<RankedFormulasInterpretation>
      */
     public MinimalRankedEntailmentCumulativeFormulaReasoner(RankConstructor<RankedFormulasInterpretation> constructor) {
         super(constructor);
