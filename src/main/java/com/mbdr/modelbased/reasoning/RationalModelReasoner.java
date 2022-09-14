c3d489f620aefdd26e48ca7b71baa8fd8212f8f9
Jaron Cohen <51699163+jaronsgit@users.noreply.github.com>
2022-09-14 15:12:35 +0200

    Update RationalModelReasoner.java

--- src/main/java/com/mbdr/modelbased/reasoning/RationalModelReasoner.java
+++ src/main/java/com/mbdr/modelbased/reasoning/RationalModelReasoner.java
@@ -2,6 +2,10 @@ package com.mbdr.modelbased.reasoning;
 
 import com.mbdr.modelbased.construction.ModelRank;
 
+/**
+ * Reasoner wrapper class used mainly for efficient benchmarking purposes.
+ * Functions as a wrapper for the minimal ranked entailment reasoner that uses ranked models produced by ModelRank.
+ */
 public class RationalModelReasoner extends MinimalRankedEntailmentReasoner{
     
     public RationalModelReasoner(){
