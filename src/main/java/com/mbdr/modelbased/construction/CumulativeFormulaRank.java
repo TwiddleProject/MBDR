806a851d98de4949b7ac24a7b06ddee30e670372
Jaron Cohen <51699163+jaronsgit@users.noreply.github.com>
2022-09-14 22:12:10 +0200

    Update CumulativeFormulaRank.java

--- src/main/java/com/mbdr/modelbased/construction/CumulativeFormulaRank.java
+++ src/main/java/com/mbdr/modelbased/construction/CumulativeFormulaRank.java
@@ -28,23 +28,15 @@ public class CumulativeFormulaRank implements RankConstructor<RankedFormulasInte
     public RankedFormulasInterpretation construct(DefeasibleKnowledgeBase knowledge) {
 
         RankedFormulasInterpretation rankedModel = new RankedFormulasInterpretation(0);
-
         Sat4jSolver solver = new Sat4jSolver();
-
         PlFormula classicalKnowledgeFormula = new Conjunction(knowledge.getPropositionalKnowledge());
-
         PlBeliefSet remainingDefeasibleFormulas = knowledge.getDefeasibleKnowledge();
-
         PlFormula F0Defeasible = new Conjunction(knowledge.getDefeasibleKnowledge());
-
         PlFormula currentRankFormula = new Conjunction(classicalKnowledgeFormula, F0Defeasible);
 
         if (solver.isSatisfiable(knowledge.union())) {
-
             int rankIndex = rankedModel.addRank(currentRankFormula);
-
             int numPreviousRemainingDefeasibleFormulas = -1;
-
 
             while (!remainingDefeasibleFormulas.isEmpty()
                     && numPreviousRemainingDefeasibleFormulas != remainingDefeasibleFormulas.size()) {
@@ -57,23 +49,17 @@ public class CumulativeFormulaRank implements RankConstructor<RankedFormulasInte
                     if (solver.isSatisfiable(Arrays.asList(currentRankFormula, antecedent))) {
                         checkedFormulas.add(formula);
                     }
-
                 }
 
                 numPreviousRemainingDefeasibleFormulas = remainingDefeasibleFormulas.size();
                 remainingDefeasibleFormulas.removeAll(checkedFormulas);
-
                 PlFormula remainingDefeasibleConjunction = new Conjunction(remainingDefeasibleFormulas);
-
                 currentRankFormula = new Conjunction(Arrays.asList(
                         classicalKnowledgeFormula,
                         remainingDefeasibleConjunction));
-
                 rankIndex = rankedModel.addRank(currentRankFormula);
             }
-
             rankedModel.setInfiniteRank(new Tautology());
-
         } else {
             rankedModel.setInfiniteRank(new Tautology());
         }
