834f0591aa8dd7c43792f09cbad7d1df3914f3fd
Jaron Cohen <51699163+jaronsgit@users.noreply.github.com>
2022-09-14 17:29:50 +0200

    Update DefeasibleParser.java

--- src/main/java/com/mbdr/utils/parsing/DefeasibleParser.java
+++ src/main/java/com/mbdr/utils/parsing/DefeasibleParser.java
@@ -7,8 +7,21 @@ import org.tweetyproject.commons.ParserException;
 
 import com.mbdr.common.structures.DefeasibleFormulaCollection;
 
+/**
+ * This is the Generic class interface to be shared by all parsers that read in knowledge base text files and return
+ * DefeasibleFormulaCollections, which serve as the basis for DefeasibleKnowledgeBases.
+ * @param <T>
+ */
 public interface DefeasibleParser<T extends DefeasibleFormulaCollection> {
-    
+
+    /**
+     * Parse method interface
+     * @param filePath - path to the knowledge base file
+     * @return extension of DefeasibleFormulaCollection
+     * @throws FileNotFoundException
+     * @throws ParserException
+     * @throws IOException
+     */
     T parse(String filePath) throws FileNotFoundException, ParserException, IOException ;
 
 }
