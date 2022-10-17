60a137baee1ca9fc828bd20ebabb3de17678c350
Carl Combrinck <carljcombrinck@gmail.com>
2022-10-17 12:16:00 +0200

    Move static file serving to Javalin

--- src/main/java/com/mbdr/web/WebApp.java
+++ src/main/java/com/mbdr/web/WebApp.java
@@ -1,6 +1,8 @@
 package com.mbdr.web;
 
 import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+
 import static io.javalin.apibuilder.ApiBuilder.*;
 
 public class WebApp {
@@ -17,6 +19,33 @@ public class WebApp {
 
     public static void main(String[] args) {
         Javalin app = Javalin.create(config -> {
+            config.staticFiles.add(staticFiles -> {
+                staticFiles.hostedPath = "/app";                   // change to host files on a subpath, like '/assets'
+                staticFiles.directory = "frontend/out/";              // the directory where your files are located
+                staticFiles.location = Location.EXTERNAL;      // Location.CLASSPATH (jar) or Location.EXTERNAL (file system)
+                staticFiles.precompress = false;                // if the files should be pre-compressed and cached in memory (optimization)
+                staticFiles.aliasCheck = null;                  // you can configure this to enable symlinks (= ContextHandler.ApproveAliases())
+                // staticFiles.headers = Map.of(...);              // headers that will be set for the files
+                staticFiles.skipFileFunction = req -> false;    // you can use this to skip certain files in the dir, based on the HttpServletRequest
+            });
+            config.staticFiles.add(staticFiles -> {
+                staticFiles.hostedPath = "/static";                   // change to host files on a subpath, like '/assets'
+                staticFiles.directory = "frontend/out/static/";              // the directory where your files are located
+                staticFiles.location = Location.EXTERNAL;      // Location.CLASSPATH (jar) or Location.EXTERNAL (file system)
+                staticFiles.precompress = false;                // if the files should be pre-compressed and cached in memory (optimization)
+                staticFiles.aliasCheck = null;                  // you can configure this to enable symlinks (= ContextHandler.ApproveAliases())
+                // staticFiles.headers = Map.of(...);              // headers that will be set for the files
+                staticFiles.skipFileFunction = req -> false;    // you can use this to skip certain files in the dir, based on the HttpServletRequest
+            });
+            config.staticFiles.add(staticFiles -> {
+                staticFiles.hostedPath = "/_next";                   // change to host files on a subpath, like '/assets'
+                staticFiles.directory = "frontend/out/_next/";              // the directory where your files are located
+                staticFiles.location = Location.EXTERNAL;      // Location.CLASSPATH (jar) or Location.EXTERNAL (file system)
+                staticFiles.precompress = false;                // if the files should be pre-compressed and cached in memory (optimization)
+                staticFiles.aliasCheck = null;                  // you can configure this to enable symlinks (= ContextHandler.ApproveAliases())
+                // staticFiles.headers = Map.of(...);              // headers that will be set for the files
+                staticFiles.skipFileFunction = req -> false;    // you can use this to skip certain files in the dir, based on the HttpServletRequest
+            });
             config.plugins.enableCors(cors -> {
                 cors.add(it -> {
                     it.allowHost("http://app.twiddleproject.com", "https://app.twiddleproject.com");
@@ -25,7 +54,7 @@ public class WebApp {
         }).start(getPort());
 
         app.get("/", ctx -> {
-            ctx.redirect("/app");
+            ctx.redirect("/app/");
         });
 
         app.routes(() -> {
