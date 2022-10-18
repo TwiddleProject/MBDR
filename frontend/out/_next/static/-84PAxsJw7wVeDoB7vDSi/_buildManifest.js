7cb558b323f05326f8a8669a83879bc038584707
Carl Combrinck <carljcombrinck@gmail.com>
2022-10-18 22:50:16 +0200

    Update production files

--- frontend/out/_next/static/-84PAxsJw7wVeDoB7vDSi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/435-6564c43528176648.js","static/css/149b18973e5508c7.css","static/chunks/pages/index-6f71d35f232a37fb.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
